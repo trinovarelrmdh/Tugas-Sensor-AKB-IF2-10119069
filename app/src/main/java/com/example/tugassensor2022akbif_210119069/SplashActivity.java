05edee0e11cdd22eb276ec289aa7e7bb18099ead
trinovarelrmdh <varelrahmadhani12@gmail.com>
2022-07-01 23:45:55 +0700

    tugas akb

---
+++ app/src/main/java/com/example/tugassensor2022akbif_210119069/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.tugassensor2022akbif_210119069;
+//Nama  : Tri Novarel Rahmadhani
+//NIM   : 10119069
+//Kelas : IF-2
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.tugassensor2022akbif_210119069.R;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread thread = new Thread(){
+            public void run() {
+                try {
+                    sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+}
